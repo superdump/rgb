f1c0f85e92521e05d0bd4354b7146e207f0ef29a
Robert Swain <robert.swain@gmail.com>
2019-05-01 12:37:58 +0200

    Vulkan Tutorial - Instance - Creating an instance

--- rgb/src/main.cpp
+++ rgb/src/main.cpp
@@ -13,6 +13,9 @@ private:
   const int HEIGHT = 600;
 
   GLFWwindow* window;
+
+  VkInstance instance;
+
 public:
   void run()
   {
@@ -37,7 +40,42 @@ private:
 
   void initVulkan()
   {
+    createInstance();
+  }
 
+  void createInstance()
+  {
+    // This is technically optional but provides useful information for the
+    // driver to optimize for our specific application
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    // Required
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    // Get the extensions required by the window system
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    // FIXME: Global validation layers disabled
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    {
+      throw std::runtime_error("Failed to create Vulkan instance!");
+    }
   }
 
   void mainLoop()
